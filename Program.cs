ded4e0fe03752fbd15482158acc7e1637a424e63
Vitaly Lukyanov <mszvibel@gmail.com>
2023-05-19 16:38:06 +0300

    Add solution

--- Program.cs
+++ Program.cs
@@ -39,4 +39,5 @@ void PrintArray(string[] array)
     for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
     Console.Write($"{array[array.Length - 1]}]");
 }
+
 PrintArray(resultArray);
